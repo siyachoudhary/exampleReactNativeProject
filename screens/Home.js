584c7b27a1a62cc20d9e7076c8389979f60012e5
Siya Choudhary <67403730+siyachoudhary@users.noreply.github.com>
2023-01-30 13:23:31 -0600

    text styling

--- screens/Home.js
+++ screens/Home.js
@@ -6,10 +6,20 @@ const Home = () => {
     const navigation = useNavigation()
   return (
     <SafeAreaView>
-      <Text>Welcome to the home page!</Text>
+      <Text style={styles.title}>Welcome to the home page!</Text>
       <Button onPress={()=>navigation.navigate("About")} title="Go to About"></Button>
     </SafeAreaView>
   )
 }
+
+const styles = StyleSheet.create({
+    title: {
+      marginTop: 50,
+      color: 'blue',
+      fontWeight: 'bold',
+      fontSize: 30,
+    }
+  });
+  
 
 export default Home
